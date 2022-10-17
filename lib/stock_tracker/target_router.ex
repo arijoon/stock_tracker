2492b4d387e44a0d25d8eea63c2ad87bc3d2ff32
Arman Yaraee <arman@yaraee.net>
2022-10-17 23:10:28 +0100

    Fix http getter to source upstream

--- lib/stock_tracker/target_router.ex
+++ lib/stock_tracker/target_router.ex
@@ -1,13 +1,13 @@
 defmodule StockTracker.TargetRouter do
   require Logger
-  # import HTTPoison, [:get]
+  import HTTPoison, [:get]
   import StockTracker.Results, [:create_result]
 
   def route(%StockTracker.Target{} = target) do
     # Default invidia implementation
     handler =
       case target.url do
-        "http://example.com" <> _ ->
+        "https://api.store.nvidia.com/" <> _ ->
           &nvidia/1
 
         _ ->
@@ -55,13 +55,4 @@ defmodule StockTracker.TargetRouter do
     Phoenix.PubSub.broadcast(:app, "result", %{ message: message, name: name })
   end
 
-  def get(_) do
-    {:ok,
-     %{
-       status_code: 200,
-       body: """
-       {"success":true,"map":null,"listMap":[{"is_active":"true","product_url":"https://www.scan.co.uk/nvidia/pr/3080ti?t\u003dkDpiSmuOPcUYq9wuiw23Xqfm4tdY19sBSxxMa1r1uUSZBjWzCqmCupNyM7hGPHyfketrwQiSGrecZXtzrFlOY1VjQmA6hoR6YCx4cH3m5y%2FEhxGTbayK32fR0ClHSTqyp3wPUslxtTdpeIimS2gQfUmBwuQY%2BX0RH%2FiBW84JeCiM7b3vXRxcbNpHX3%2B6O89rxVuG492fxrsKJ4S1HkSUHtrc1pmj9GEXNbZ8uF578TVHYYiEsFtsTpMRmr3qJnM4iPNSGcDVKeMISVNtgPQHjSwdGndaOOl3L04s%2FN3PnvAk0ob87RKdbJR3bMCv2Fb1NyPvMTijwoGxiIXRqPz5kA%3D%3D","price":"929.004","fe_sku":"NVGFT080T_UK","locale":"UK"}]}
-       """
-     }}
-  end
 end
